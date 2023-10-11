eebc1ff6d4f0576126b79e1d099ab2834238dedb
PAULO SERGIO <115193470+PSREU@users.noreply.github.com>
2023-10-11 15:16:18 -0300

    Add files via upload
    sons

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AluraMidi
+# EfeitosSonorosRadioGremio
+Mesa de mídias com áudios para efeitos sonoros especiais da rádio Grêmio Estudantil
---
+++ main.js
@@ -0,0 +1,4 @@
+function som_tecla_grilo (); {
+    document.querySelector("#som_tecla_grilo").play ();
+}
+document.querySelector(".tecla_grilo") .onclick = TocaSomGrilo
